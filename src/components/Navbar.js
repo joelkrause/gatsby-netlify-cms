8a8404a93a21b38ac23516122eadd56def03891e
Joel Krause <joel.krause8@gmail.com>
2019-02-08 17:31:29 +1100

    mobile nav progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Navbar = class extends React.Component {
-
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
- 
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
- 
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         const $target = document.getElementById(target);
- 
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
- 
-       });
-     });
-   }
- }
  
  render() {
    return (
@@ -37,6 +14,11 @@ const Navbar = class extends React.Component {
       </Link>
       </div>
       <nav>
+        <input type="checkbox" id="navbar__burger" className="navbar__burger"/>
+        <label for="navbar__burger" className="navbar__burger">
+          <span></span>
+          <span></span>
+        </label>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/blog">Posts</Link></li>
